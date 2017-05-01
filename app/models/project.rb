42a9371fd13c0fb8ac7d9f40f9deacbaa8f69e93
ulises-morales <ulises.m.025@gmail.com>
2017-04-30 19:04:26 -0700

    Implemented image defaults for portfolio items

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,4 +7,11 @@ class Project < ApplicationRecord
 
   # scope using a lamda
   scope :ruby_on_rails_projects, -> { where(subtitle: 'Ruby on Rails') }
+
+  after_initialized :set_defaults
+
+  def set_defaults
+    self.main_image ||= "http://placehold.it/600x400"
+    self.thumb_image ||= "http://placehold.it/350x200"
+  end
 end
